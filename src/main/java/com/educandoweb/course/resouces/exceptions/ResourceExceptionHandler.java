aa3f0924b823a5599850258c0d35d5fbfa404ba6
Barbara Sousa <83707213+BarbaraPSousa@users.noreply.github.com>
2021-07-12 11:59:52 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/resouces/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resouces/exceptions/ResourceExceptionHandler.java
@@ -2,7 +2,6 @@ package com.educandoweb.course.resouces.exceptions;
 
 import java.time.Instant;
 
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
@@ -10,15 +9,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice //pega as exceptions para receber tratamentos
 public class ResourceExceptionHandler {
 
-	@ExceptionHandler(ResourceNotFoundException.class)//trata as exceptions que foi lancadas
+	@ExceptionHandler(ResourceNotFoundException.class)//metodo que trata as exceptions que foi lancadas para id não existente
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resource not fond";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)//metodo que trata as exceptions que foi lancadas para id com pedidos
+	public ResponseEntity<StandardError> dataBase(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service//registando class como serviço
@@ -30,8 +33,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {// deleta Usuario com o Id
-		repository.deleteById(id);
-		
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {//tratando exceptions de deleta id que não exist
+			throw new ResourceNotFoundException(id);
+		} 
+		catch (DataIntegrityViolationException e) {//tratando exception de deleta id com pedido
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {//atualizando usuario passando id
